d4486cb5720cbb01c328da5f79ea98d2295b7f1c
VeeTTek <veettek@gmail.com>
2026-05-04 14:40:44 +0200

    Angular material table
    Add Angular material style table in ProductList.
    Change bindings to signals.
    Remove server side rendering.

--- product-catalog-angular/src/app/components/product-list/product-list.ts
+++ product-catalog-angular/src/app/components/product-list/product-list.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
 import { Product } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatProgressBarModule,
+    MatIconModule,
+  ],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
-  products: Product[] = [];
-  loading = false;
-  error: string | null = null;
+  products = signal<Product[]>([]);
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  displayedColumns = ['index', 'kod', 'nazwa', 'cena'];
 
   constructor(private productService: ProductService) { }
 
@@ -21,16 +31,20 @@ export class ProductList {
   }
 
   load(): void {
-    this.loading = true;
-    this.error = null;
+    console.log("load()");
+    this.loading.set(true);
+    this.error.set(null);
     this.productService.getAll().subscribe({
       next: (data) => {
-        this.products = data;
-        this.loading = false;
+        console.log("next");
+        console.log(data);
+        this.products.set(data);
+        this.loading.set(false);
       },
       error: () => {
-        this.error = 'Nie udało się pobrać produktów.';
-        this.loading = false;
+        console.log("error");
+        this.error.set('Nie udało się pobrać produktów.');
+        this.loading.set(false);
       }
     });
   }
